b25151a4345206ea6013a474df6f4cb9f26eb339
schriftgestalt <georg.seifert@mac.com>
2022-11-04 10:28:14 +0100

    check for required keys

--- test.py
+++ test.py
@@ -2,11 +2,21 @@
 #
 
 import sys
-from pbPlist import pbPlist
-
 try:
-	# purposely throwing away the return of PBPlist since we're just checking that it parses
+	from Foundation import NSDictionary
+	z = NSDictionary.dictionaryWithContentsOfFile_("packages.plist")
+except:
+	from pbPlist import pbPlist
 	z = pbPlist.PBPlist("packages.plist")
-except Exception as e:
-	print(e)
-	sys.exit(1)
+
+#print(type(z["packages"]))
+packageDict = z["packages"]
+for packageType in packageDict.keys():
+	packages = packageDict[packageType]
+	for package in packages:
+		if not (package.get("title", None) or package.get("titles", None)):
+			print(f"missing title in {package}")
+		if not package.get("url", None):
+			print(f"missing url in {package}")
+		if not package.get("path", None) and packageType == "plugins":
+			print(f"missing path in {package}")
